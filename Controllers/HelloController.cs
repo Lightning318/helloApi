6d7608f70f7d5d6b934b819604b7d8a4be586aa7
Jonathan Bull <jonathan.bull@ibm.com>
2020-02-17 15:39:24 -0600

    initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Hello world! v1";
+    }
+}
